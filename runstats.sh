7231b77043224dc24ad0e3f67a76998f4b27f8bb
Andrew Moore <amoore@cpan.org>
2009-03-19 15:31:28 -0500

    making runstats available for more than one domain

--- runstats.sh
+++ runstats.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+set -x
+
+if [[ -z $1 ]] ; then
+  SITE=andrewmoore.com
+else
+  SITE=$1
+fi
+
 VISITORS=/usr/bin/visitors
-ACCESSLOG=/var/log/apache2/andrewmoore.com/access.log*
+ACCESSLOG=/var/log/apache2/$SITE/access.log*
 WORKDIR=/home/amoore/public_html
 OUTPUT=$WORKDIR/stats.html
-PREFIX="--prefix http://andrewmoore.com --prefix http://www.andrewmoore.com "
+PREFIX="--prefix http://$SITE --prefix http://www.$SITE "
 ZCAT="/bin/zcat -f"
 EXCLUDE="--exclude private --exclude 65.165.109.82 --exclude cpe-24-163-202-170.kc.res.rr.com --exclude limbo.mooresystems.com --exclude /wiki/skins --exclude usemsgcache --exclude action=raw --exclude TheKCGuy_daily_rss.xml --exclude skins"
 

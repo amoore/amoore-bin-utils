d39abc6a8e0011aea97091762e7227aba44d5b25
Andrew Moore <amoore@cpan.org>
2010-10-28 13:38:40 -0500

    changed name from limbo to cake

--- runstats.sh
+++ runstats.sh
@@ -14,7 +14,7 @@ WORKDIR=/home/amoore/public_html
 OUTPUT=$WORKDIR/stats.html
 PREFIX="--prefix http://$SITE --prefix http://www.$SITE "
 ZCAT="/bin/zcat -f"
-EXCLUDE="--exclude private --exclude 65.165.109.82 --exclude cpe-24-163-202-170.kc.res.rr.com --exclude limbo.mooresystems.com --exclude /wiki/skins --exclude usemsgcache --exclude action=raw --exclude TheKCGuy_daily_rss.xml --exclude skins"
+EXCLUDE="--exclude private --exclude 65.165.109.82 --exclude cpe-24-163-202-170.kc.res.rr.com --exclude cake.mooresystems.com --exclude /wiki/skins --exclude usemsgcache --exclude action=raw --exclude TheKCGuy_daily_rss.xml --exclude skins"
 
 $ZCAT $ACCESSLOG | \
           $VISITORS \
